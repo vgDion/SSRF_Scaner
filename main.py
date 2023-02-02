bbb5e0ff4e2a725c63959c5c3a1f40c0e737ed2a
DIon <78384263+vgDion@users.noreply.github.com>
2023-02-02 18:11:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from burp import IBurpExtender
 from burp import IHttpListener
-
-
+import urlparse
 
 class BurpExtender(IBurpExtender, IHttpListener):
     def registerExtenderCallbacks(self, callbacks):
-        # Get a reference to the helpers object
+        
         self._helpers = callbacks.getHelpers()
-
-        # Set the extension name
+        
+      
         callbacks.setExtensionName("SSRF Vulnerability Scanner")
-
-        # Register the HTTP listener
+        
+        
         callbacks.registerHttpListener(self)
-
+    
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
-        # Only process requests, not responses
+        
         if not messageIsRequest:
             return
-
-        # Get the request and request information
+        
+        
         request = messageInfo.getRequest()
         requestInfo = self._helpers.analyzeRequest(request)
-
-        # Check if the request is vulnerable to SSRF
+        
+       
         if self.checkForSSRF(requestInfo):
-            # Report the vulnerability to Burp
-            self.callbacks.addScanIssue(self.ScanIssue(
+            
+            callbacks.addScanIssue(ScanIssue(
                 messageInfo.getHttpService(),
                 self._helpers.analyzeRequest(messageInfo).getUrl(),
                 [self._callbacks.getScanIssues("SSRF Vulnerability")],
@@ -34,27 +33,34 @@ class BurpExtender(IBurpExtender, IHttpListener):
                 "The request is vulnerable to SSRF",
                 "High",
                 "Certain"))
-
+    
     def checkForSSRF(self, requestInfo):
-        # Extract the URL from the request
-        url = requestInfo.getUrl().toString()
-
-        # Check for known blacklisted IP addresses or localhost addresses
-        if "localhost" in url or "127.0.0.1" in url:
+       
+        host = requestInfo.getUrl().getHost()
+        parsed_host = urlparse.urlparse(host).hostname
+        if parsed_host == "127.0.0.1" or parsed_host == "localhost":
             return True
-
-        # Check for known file URIs, e.g. file://
-        if "file://" in url:
+        
+        
+        if self.checkForPrivateIP(parsed_host):
             return True
-
-        # Check for known protocol that shouldn't be used in requests
-        blacklisted_protocols = ["gopher", "dict", "dict://", "tp://", "tftp://", "ldap://", "ldaps://", "rtsp://"]
-        for protocol in blacklisted_protocols:
-            if protocol in url:
-                return True
-
-        # Check for external IP address
-
-
-        # If none of the checks above return True, then the request is not vulnerable to SSRF
+        
+        
+        if self.checkForReservedIP(parsed_host):
+            return True
+           
+        
+        return False
+    
+    
+    def checkForPrivateIP(self, host):
+       
+        if host.startswith("10.") or host.startswith("172.") or host.startswith("192.168."):
+            return True
+        return False
+    
+    def checkForReservedIP(self, host):
+        
+        if host.startswith("0.") or host.startswith("169.254.") or host.startswith("192.") or host.startswith("198.18."):
+            return True
         return False
